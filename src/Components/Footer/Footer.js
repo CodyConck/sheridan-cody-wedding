098407ebfbbfc38f943cb05b08803e1fb1c9d8b9
CodyConck <codyconck@gmail.com>
2021-12-31 11:01:06 -0600

    added footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () =>{
+    return(
+        <nav>
+            <div className="div-footer">
+                <p>S&C</p>
+            </div>
+        </nav>
+    
+    )
+}
+
+export default Footer;

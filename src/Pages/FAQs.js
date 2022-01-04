9b753841473ae175cc2ba7f6d9aa97828a920245
CodyConck <codyconck@gmail.com>
2022-01-04 16:34:15 -0600

    added functional navbar and pages

---
+++ src/Pages/FAQs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FAQs = () => {
+    return (
+        <div class='container-fluid'>
+        <h1>FAQs</h1>
+        </div> 
+    )
+}
+
+export default FAQs
---
+++ src/Pages/WeddingDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeddingDetails = () => {
+    return (
+        <div class='container-fluid'>
+        <h1>WEDDING DETAILS</h1>
+        </div> 
+    )
+}
+
+export default WeddingDetails

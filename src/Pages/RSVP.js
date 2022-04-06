19fb3981b74cee44d70416d11f9e0b3c15eadb79
CodyConck <codyconck@gmail.com>
2022-04-06 14:08:36 -0500

    updated rsvp page with input field and submit button

--- src/Pages/RSVP.js
+++ src/Pages/RSVP.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { InputGroup } from 'react-bootstrap'
+import { FormControl } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
 
 const RSVP = () => {
     return (
-        <div class='container-fluid'>
-        <h1>RSVP</h1>
-        </div> 
+        <div class='container-fluid' className='rsvp-style'>
+            <h1>You're Invited</h1>
+            <InputGroup className="mb-3">
+                <InputGroup.Text>First and last name on invitation</InputGroup.Text>
+                <FormControl aria-label="First name" />
+                <FormControl aria-label="Last name" />
+            </InputGroup>
+            <>
+                <Button as="input" type="submit" value="Submit" />{' '}
+                
+            </>
+        </div>
     )
 }
 

5ba5be1584cf0b86394bd302ecf7384b8ed01820
CodyConck <codyconck@gmail.com>
2022-04-06 13:26:50 -0500

    added home page body and message, background color

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,20 +2,29 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div id='Home' style={{
+        <div className='Home' style={{
             display: 'flex',
+            flexDirection:'column',
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            paddingBottom: '25px'
         }}>
             <img src="/images/IMG_0523.JPG" alt="engagement pic"
                 style={{
                     height: 400,
                     width: 300,
+                    borderRadius: 10
                 }}
             />
             <div className='home-page-message' >
                 <h3>Please join us for a our wedding celebration on</h3>
                 <h1>Some Day 2022 or 2023</h1>
+            </div>
+            <div className='home-page-body'>
+                <h1>We can't wait to celebrate with you!</h1>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
         </div>
 

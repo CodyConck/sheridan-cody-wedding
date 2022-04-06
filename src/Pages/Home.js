1ee8a4ba436d9650e6f4d0c3562030883f3b336d
CodyConck <codyconck@gmail.com>
2022-04-06 13:02:22 -0500

    added text and styling to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,16 +3,23 @@ import React from 'react'
 const Home = () => {
     return (
         <div id='Home' style={{
-            display: 'flex', 
-            alignItems:'center',
-            justifyContent: 'center'}}>
-        <img src="/images/IMG_0523.JPG" alt="engagement pic"
-        style={{
-            height: 400, 
-            width: 300, 
-            }}
-        />
-        </div> 
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <img src="/images/IMG_0523.JPG" alt="engagement pic"
+                style={{
+                    height: 400,
+                    width: 300,
+                }}
+            />
+            <div className='home-page-message' >
+                <h3>Please join us for a our wedding celebration on</h3>
+                <h1>Some Day 2022 or 2023</h1>
+            </div>
+        </div>
+
+
     )
 }
 
